51d64bee581f88966bf08318a70ddc0c3baf2653
Sean-McKenzie <sean_d_mckenzie@yahoo.com>
2024-05-27 21:52:25 -0500

    More Deployment fixes

--- src/client/components/SingleTrack.jsx
+++ src/client/components/SingleTrack.jsx
@@ -15,7 +15,7 @@ export default function SingleTrack() {
     const fetchReviews = async () => {
       try {
         const response = await fetch(
-          `http://tune_talk_user:WpfPaFxhqDgwK9j012wcQqgnBZq6MIzZ@dpg-cpahv1m3e1ms739q90bg-a.ohio-postgres.render.com/tune_talk:3000/api/reviews/topsongs/${trackId}`
+          `http://dpg-cpahv1m3e1ms739q90bg-a.ohio-postgres.render.com/tune_talk:3000/api/reviews/topsongs/${trackId}`
         );
         const reviewsData = await response.json();
         setReviews(reviewsData);
