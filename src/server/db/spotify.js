55c94361017855bc8f408adb269fe1790cb950a9
Sean-McKenzie <sean_d_mckenzie@yahoo.com>
2024-05-29 20:09:37 -0500

    deploy fixes

--- src/server/db/spotify.js
+++ src/server/db/spotify.js
@@ -132,6 +132,7 @@ const fetchManyAlbumsInfo = async (id) => {
 
 const searchArtist = async (artistName) => {
   try {
+     console.log("made it");
     const response = await axios.get(
       `https://api.spotify.com/v1/search?q=${artistName}&type=artist`,
       {
